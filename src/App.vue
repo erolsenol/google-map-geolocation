d7b5aa56d51b6ccccb0a9ee26a3c8355228b6f76
Erol Senol <erolsenol@outlook.com>
2023-04-29 18:45:37 +0300

    Service table component add

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <ServiceTable />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ServiceTable from './components/ServiceTable.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
+  components: { ServiceTable },
 }
 </script>
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,9 +2,10 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { VDataTable } from 'vuetify/labs/VDataTable'
 
 const vuetify = createVuetify({
-  components,
+  components: { ...components, VDataTable },
   directives,
 })
 
