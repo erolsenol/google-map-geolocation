a681a9ab0b4c16851f859bb09e20215be684e3bd
Erol Senol <erolsenol@outlook.com>
2023-05-04 01:52:11 +0300

    command line removed

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,6 @@ import mutations from './mutations'
 import actions from './actions'
 import getters from './getters'
 
-// Create a new store instance.
 const store = createStore({ state, mutations, actions, getters })
 
 export default store
